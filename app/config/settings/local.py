b32f5168c152fe58b4bc9804b1deedbd65f70852
Sehyeon Youn <d.sehyeon@gmail.com>
2018-09-08 18:12:32 +0900

    media root add

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -18,3 +18,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+INSTALLED_APPS += [
+    'django_extensions'
+]
--- app/members/models.py
+++ app/members/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """
+    Default User models
+    """
     email = models.EmailField(unique=True)
     profile_image = models.ImageField(upload_to='members_profile')
     is_facebook = models.BooleanField(default=False, blank=True)
