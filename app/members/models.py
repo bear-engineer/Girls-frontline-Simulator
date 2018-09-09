c7b8761412cbd8af278f5de8cf4f2c04edef305d
Sehyeon Youn <d.sehyeon@gmail.com>
2018-09-09 20:34:30 +0900

    nikname field add

--- app/members/models.py
+++ app/members/models.py
@@ -6,7 +6,8 @@ class User(AbstractUser):
     """
     Default User models
     """
+    display_name = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    profile_image = models.ImageField(upload_to='members_profile')
+    profile_image = models.ImageField(upload_to='members_profile', blank=True)
     is_facebook = models.BooleanField(default=False, blank=True)
     is_kakao = models.BooleanField(default=False, blank=True)
