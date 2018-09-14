369011118b443bd0f342226483e10ad32d809e4e
Sehyeon Youn <dev.younlab@gmail.com>
2018-09-14 11:27:17 +0900

    doll models 분리 및 db저장 완료

--- app/tactical_dolls/models/doll.py
+++ app/tactical_dolls/models/doll.py
@@ -27,13 +27,14 @@ class Doll(models.Model):
 
     doll_id = models.PositiveIntegerField(verbose_name='인형 품번', primary_key=True, unique=True)
     name = models.CharField(max_length=50, verbose_name='인형 이름')
-    krname = models.CharField(max_length=50, verbose_name='인형 이름(한국어)', blank=True, null=True)
+    kr_name = models.CharField(max_length=50, verbose_name='인형 이름(한국어)', blank=True, null=True)
     rank = models.CharField(max_length=1, choices=DOLL_RATING, verbose_name='인형 등급', blank=True, null=True)
     type = models.CharField(max_length=3, choices=DOLL_TYPE, verbose_name='인형 유형', blank=True, null=True)
     illust = models.CharField(max_length=50, verbose_name='일러스트레이터', blank=True, null=True)
     voice = models.CharField(max_length=50, verbose_name='성우', blank=True, null=True)
     build_time = models.PositiveSmallIntegerField(verbose_name='제조 시간', blank=True, null=True)
     is_upgrade = models.BooleanField(default=False, verbose_name='개조', blank=True)
+
     # doll_thumbnail_image = models.ImageField(upload_to='doll_thumbnail_images', verbose_name='아이콘 일러스트')
     # doll_default_image = models.ImageField(upload_to='doll_default_images', verbose_name='기본 일러스트')
     # doll_damage_image = models.ImageField(upload_to='doll_damage_images', verbose_name='파손 일러스트')
--- app/tactical_dolls/views.py
+++ app/tactical_dolls/views.py
@@ -6,7 +6,8 @@ from tactical_dolls.models import Doll
 
 
 class Update(View):
-    def get(self, request):
+    def get(request, *args, **kwargs):
+
         data_source = requests.get(
             'https://raw.githubusercontent.com/36base/girlsfrontline-core/master/data/doll.json'
         ).json()
@@ -24,82 +25,90 @@ class Update(View):
                 armor = 0
 
             try:
-                doll_range = source['stats']['range']
+                range = source['stats']['range']
             except KeyError:
-                doll_range = 0
+                range = 0
 
             try:
-                doll_shield = source['stats']['shield']
+                shield = source['stats']['shield']
             except KeyError:
-                doll_shield = 0
+                shield = 0
 
             try:
-                doll_critdmg = source['stats']['critDmg']
+                critdmg = source['stats']['critDmg']
             except KeyError:
-                doll_critdmg = 0
+                critdmg = 0
 
             try:
-                doll_bullet = source['stats']['bullet']
+                bullet = source['stats']['bullet']
             except KeyError:
-                doll_bullet = 0
+                bullet = 0
             try:
                 build_time = source['buildTime']
             except KeyError:
                 build_time = 0
 
             try:
-                doll_illustrator_creator = source['illust']
+                illustrator_creator = source['illust']
             except KeyError:
-                doll_illustrator_creator = None
+                illustrator_creator = None
 
             try:
-                doll_cv = source['voice']
+                voice = source['voice']
             except KeyError:
-                doll_cv = None
+                voice = None
 
             try:
-                doll_krname = source['krName']
+                krname = source['krName']
             except KeyError:
-                doll_krname = None
+                krname = None
             try:
-                doll_nightview = source['doll_nightview']
+                night_view = source['night_view']
             except KeyError:
-                doll_nightview = 0
+                night_view = 0
             try:
-                doll_cooldown = source['doll_cooldown']
+                cool_down = source['cool_down']
             except KeyError:
-                doll_cooldown = 0
+                cool_down = 0
 
-            dolls_data = {
-                'doll_krname': doll_krname,
-                'doll_no': source['id'],
-                'doll_manufacturing_time': build_time,
-                'doll_rating': source['rank'],
-                'doll_type': source['type'].upper(),
-                'doll_illustrator_creator': doll_illustrator_creator,
-                'doll_cv': doll_cv,
-                'doll_hp': source['stats']['hp'],
-                'doll_eva': source['stats']['dodge'],
-                'doll_fp': source['stats']['pow'],
-                'doll_acc': source['stats']['hit'],
-                'doll_speed': source['stats']['speed'],
-                'doll_rate': source['stats']['rate'],
-                'doll_armorpiercing': armor_piercing,
-                'doll_crit': source['stats']['crit'],
-                'doll_armor': armor,
-                'doll_range': doll_range,
-                'doll_shield': doll_shield,
-                'doll_bullet': doll_bullet,
-                'doll_critdmg': doll_critdmg,
-                'doll_nightview': doll_nightview,
-                'doll_cooldown': doll_cooldown,
+            doll_data = {
+                'kr_name': krname,
+                'doll_id': source['id'],
+                'build_time': build_time,
+                'rank': source['rank'],
+                'type': source['type'].upper(),
+                'illust': illustrator_creator,
+                'voice': voice,
             }
-            dolls, dolls_create = Doll.objects.update_or_create(
-                doll_name=source['name'],
-                defaults=dolls_data,
+
+            doll_status_data = {
+                'hp': source['stats']['hp'],
+                'dodge': source['stats']['dodge'],
+                'pow': source['stats']['pow'],
+                'hit': source['stats']['hit'],
+                'speed': source['stats']['speed'],
+                'rate': source['stats']['rate'],
+                'armor_piercing': armor_piercing,
+                'crit': source['stats']['crit'],
+                'armor': armor,
+                'range': range,
+                'shield': shield,
+                'bullet': bullet,
+                'critdmg': critdmg,
+                'night_view': night_view,
+                'cool_down': cool_down,
+            }
+
+            doll, doll_create = Doll.objects.update_or_create(
+                name=source['name'],
+                defaults=doll_data,
             )
 
-            dolls.save()
+            doll.doll_status.update_or_create(
+                hp=source['stats']['hp'],
+                defaults=doll_status_data,
+            )
+            doll.save()
             print(f"{source['name']} 저장 성공")
 
-        return HttpResponse(data_source, content_type='application/json')
+        return HttpResponse('update')
