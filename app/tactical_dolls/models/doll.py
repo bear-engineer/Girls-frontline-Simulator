061c84554ad9a490656a89b2e64ca20744424808
Sehyeon Youn <dev.younlab@gmail.com>
2018-09-17 10:19:23 +0900

    바뀐 데이터 형식으로 모델 수정중

--- app/tactical_dolls/models/doll.py
+++ app/tactical_dolls/models/doll.py
@@ -9,33 +9,25 @@ class Doll(models.Model):
     """
     전술 인형 정보
     """
-    DOLL_TYPE = (
-        ('hg', 'HG'),
-        ('smg', 'SMG'),
-        ('rf', 'RF'),
-        ('ar', 'AR'),
-        ('mg', 'MG'),
-        ('sg', 'SG'),
-    )
-    DOLL_RATING = (
-        ('1', '★'),
-        ('2', '★★'),
-        ('3', '★★★'),
-        ('4', '★★★★'),
-        ('5', '★★★★★'),
+    RANK_CHOICE = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
     )
 
-    doll_id = models.PositiveIntegerField(verbose_name='인형 품번', primary_key=True, unique=True)
-    name = models.CharField(max_length=50, verbose_name='인형 이름')
-    kr_name = models.CharField(max_length=50, verbose_name='인형 이름(한국어)', blank=True, null=True)
-    rank = models.CharField(max_length=1, choices=DOLL_RATING, verbose_name='인형 등급', blank=True, null=True)
-    type = models.CharField(max_length=3, choices=DOLL_TYPE, verbose_name='인형 유형', blank=True, null=True)
-    illust = models.CharField(max_length=50, verbose_name='일러스트레이터', blank=True, null=True)
-    voice = models.CharField(max_length=50, verbose_name='성우', blank=True, null=True)
-    build_time = models.PositiveSmallIntegerField(verbose_name='제조 시간', blank=True, null=True)
-    is_upgrade = models.BooleanField(verbose_name='개조', blank=True)
+    TYPE_CHOICE = (
+        ('hg', 'hg'),
+        ('ar', 'ar'),
+        ('smg', 'smg'),
+        ('sg', 'sg'),
+        ('mg', 'mg'),
+        ('rf', 'rf'),
+    )
 
-    # doll_thumbnail_image = models.ImageField(upload_to='doll_thumbnail_images', verbose_name='아이콘 일러스트')
-    # doll_default_image = models.ImageField(upload_to='doll_default_images', verbose_name='기본 일러스트')
-    # doll_damage_image = models.ImageField(upload_to='doll_damage_images', verbose_name='파손 일러스트')
-    # doll_Introduce = models.TextField(max_length=255, verbose_name='소개', blank=True)
+    id = models.PositiveSmallIntegerField(unique=True, primary_key=True)
+    rank = models.CharField(max_length=1, choices=RANK_CHOICE)
+    type = models.CharField(max_length=3, choices=TYPE_CHOICE)
+    buildtime = models.PositiveSmallIntegerField(blank=True, null=True)
+    codename = models.CharField(max_length=50, blank=True, null=True)
