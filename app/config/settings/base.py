7da8dc5a2286fd0e14a80696fefb9513352573d9
Sehyeon Youn <dev.younlab@gmail.com>
2018-09-18 01:25:17 +0900

    django rest modult add

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
