d5ca5f9108c336b4a2342d228d80df16fa296ea8
Sehyeon Youn <dev.younlab@gmail.com>
2018-09-27 14:13:22 +0900

    utils doll postions 모듈분리

--- app/utils/formula.py
+++ app/utils/formula.py
@@ -1,24 +1,20 @@
 from tactical_dolls.models import DollEffect, DollEffectGrid, DollEffectPos
 
 
-def doll_effect(**kwargs):
+def doll_position(**kwargs):
+    """
+    re position
+    :param kwargs:
+    :return:
+    """
     id = kwargs.get('id')
     center = kwargs.get('center')
     doll_effect = [item for item in DollEffect.objects.filter(doll__id=id)][0]
     doll_effect_pos = [item for item in DollEffectPos.objects.filter(doll__id=id)]
-    doll_effect_grid = [item for item in DollEffectGrid.objects.filter(doll__id=id)][0]
     doll_positions = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
+        'center': None,
+        'pos': []
     }
-    doll_postion_grid = {}
 
     # 포지션 위치값
     if center > doll_effect.center:
@@ -28,7 +24,8 @@ def doll_effect(**kwargs):
             pos_values = pos.pos + values
             if pos_values < 9:
                 pos_values = 0
-            doll_positions[center_position].append(pos_values)
+            doll_positions['center'] = center_position
+            doll_positions['pos'].append(pos_values)
     elif center < doll_effect.center:
         values = doll_effect.center - center
         center_position = doll_effect.center - values
@@ -36,10 +33,12 @@ def doll_effect(**kwargs):
             pos_values = pos.pos - values
             if pos_values < 0:
                 pos_values = 0
-            doll_positions[center_position].append(pos_values)
+            doll_positions['center'] = center_position
+            doll_positions['pos'].append(pos_values)
     else:
         # default center 일 경우
-        doll_positions[center] = [item.pos for item in doll_effect_pos]
+        doll_positions['center'] = center
+        doll_positions['pos'] = [item.pos for item in doll_effect_pos]
     return doll_positions
 
 
