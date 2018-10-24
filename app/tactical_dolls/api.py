c610e1d1952dacc88104f307fc123c5af53635db
Sehyeon Youn <dev.younlab@gmail.com>
2018-10-24 14:11:28 +0900

    api fix

--- app/tactical_dolls/api.py
+++ app/tactical_dolls/api.py
@@ -1,13 +1,7 @@
-import json
-
 from rest_framework import generics
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from utils.formula import Formula
-# from utils.formula import EffectFormula as Formula
 from .serializer import DollSerializer, DollDetailSerializer
 from .models import Doll
 
@@ -30,20 +24,3 @@ class DollFormula(APIView):
 
     def get(self, request, *args, **kwargs):
         return Response('get')
-# @csrf_exempt
-# def postman(request):
-#     data = JSONParser().parse(request)
-#     response = JSONRenderer().render(Formula(data).status_equip_effect_formula())
-#
-#     return HttpResponse(response, content_type='application/json')
-#
-#
-# class TestView(APIView):
-#     def get(self, request, *args, **kwargs):
-#         print(request.data)
-#         return Response(request.data)
-#
-#     def post(self, request, *args, **kwargs):
-#         data = JSONRenderer().render(request.data)
-#         print(data)
-#         return Response(request.data)
