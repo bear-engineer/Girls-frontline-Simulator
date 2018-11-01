4a730e32b7f63cebec1ea289dfa058c9b8d91fc8
Sehyeon Youn <dev.younlab@gmail.com>
2018-11-01 20:08:32 +0900

    스테이터스 계산식 완료

--- app/utils/simulator/__init__.py
+++ app/utils/simulator/__init__.py
@@ -1,33 +1,183 @@
 from django.utils.functional import cached_property
-
+import math
 from doll.models import Doll
 from equip.models import Equip
 
 
+class Status:
+    def __init__(self, query):
+        self.query = query
+        self.grow_data = {
+            "after100": {
+                "basic": {
+                    "armor": [13.979, 0.04],
+                    "hp": [96.283, 0.138]
+                },
+                "grow": {
+                    "dodge": [0.075, 22.572],
+                    "hit": [0.075, 22.572],
+                    "pow": [0.06, 18.018],
+                    "rate": [0.022, 15.741]
+                }
+            },
+            "normal": {
+                "basic": {
+                    "armor": [2, 0.161],
+                    "dodge": [5],
+                    "hit": [5],
+                    "hp": [55, 0.555],
+                    "pow": [16],
+                    "rate": [45],
+                    "speed": [10]
+                },
+                "grow": {
+                    "dodge": [0.303, 0],
+                    "hit": [0.303, 0],
+                    "pow": [0.242, 0],
+                    "rate": [0.181, 0]
+                }
+            }
+        }
+        self.attr_data = {
+            "hg": {
+                "hp": 0.6,
+                "pow": 0.6,
+                "rate": 0.8,
+                "speed": 1.5,
+                "hit": 1.2,
+                "dodge": 1.8
+            },
+            "smg": {
+                "hp": 1.6,
+                "pow": 0.6,
+                "rate": 1.2,
+                "speed": 1.2,
+                "hit": 0.3,
+                "dodge": 1.6
+            },
+            "rf": {
+                "hp": 0.8,
+                "pow": 2.4,
+                "rate": 0.5,
+                "speed": 0.7,
+                "hit": 1.6,
+                "dodge": 0.8
+            },
+            "ar": {
+                "hp": 1,
+                "pow": 1,
+                "rate": 1,
+                "speed": 1,
+                "hit": 1,
+                "dodge": 1
+            },
+            "mg": {
+                "hp": 1.5,
+                "pow": 1.8,
+                "rate": 1.6,
+                "speed": 0.4,
+                "hit": 0.6,
+                "dodge": 0.6
+            },
+            "sg": {
+                "hp": 2.0,
+                "pow": 0.7,
+                "rate": 0.4,
+                "speed": 0.6,
+                "hit": 0.3,
+                "dodge": 0.3,
+                "armor": 1
+            }
+        }
+
+    @cached_property
+    def formula(self):
+        level = 100
+        favor = 100
+        dummy = 5
+
+        for item in self.query:
+
+            status_list = 'hp,armor,hit,pow,dodge,rate'.split(',')
+            doll_type = item['doll_info']['type'].lower()
+            grow = item['doll_info']['grow']
+            for stats in status_list:
+                doll_status = float(item['doll_info'][f'status__{stats}'])
+                if stats == 'hp' or stats == 'armor':
+                    try:
+                        attr_data = float(self.attr_data[doll_type][stats])
+                    except KeyError:
+                        continue
+                    if level <= 100:
+                        basic_data0 = float(self.grow_data['normal']['basic'][stats][0])
+                        basic_data1 = float(self.grow_data['normal']['basic'][stats][1])
+                        basic_formula = math.ceil(
+                            (basic_data0 + ((level - 1) * basic_data1)) * attr_data * doll_status / 100
+                        )
+                        item['doll_info'][f'status__{stats}'] = basic_formula * dummy
+                    else:
+                        basic_data0 = float(self.grow_data['after100']['basic'][stats][0])
+                        basic_data1 = float(self.grow_data['after100']['basic'][stats][1])
+                        basic_formula = math.ceil(
+                            (basic_data0 + ((level - 1) * basic_data1)) * attr_data * doll_status / 100
+                        )
+                        item['doll_info'][f'status__{stats}'] = basic_formula * dummy
+                else:
+                    attr_data = float(self.attr_data[doll_type][stats])
+
+                    grow_data0 = float(self.grow_data['after100']['grow'][stats][0])
+                    grow_data1 = float(self.grow_data['after100']['grow'][stats][1])
+
+                    normal_basic = self.grow_data['normal']['basic'][stats]
+
+                    normal_basic_formula = math.ceil(
+                        normal_basic[0] * attr_data * doll_status / 100
+                    )
+                    normal_grow_formula = math.ceil(
+                        (grow_data1 + ((level - 1) * grow_data0)) * attr_data * doll_status * grow / 100 / 100
+                    )
+
+                    item['doll_info'][f'status__{stats}'] = normal_basic_formula + normal_grow_formula
+
+        return self.query
+
+
 class Simulator:
-    """
-    Data values
-    [
-        {
-            'id':1,
-            'position':5,
-            'slot_01':None,
-            'slot_02':None,
-            'slot_03':None,
-        },
-        {
-            'id':2,
-            'position':7,
-            'slot_01':None,
-            'slot_02':None,
-            'slot_03':None,
-        },
-    ]
-    """
 
     def __init__(self, data):
         self.data = data
-        self.doll_query_set = Doll.objects.filter(id__in=[item['id'] for item in self.data]).prefetch_related(
+        self.doll_grow = {
+            "after100": {
+                "basic": {
+                    "armor": [13.979, 0.04],
+                    "hp": [96.283, 0.138]
+                },
+                "grow": {
+                    "dodge": [0.075, 22.572],
+                    "hit": [0.075, 22.572],
+                    "pow": [0.06, 18.018],
+                    "rate": [0.022, 15.741]
+                }
+            },
+            "normal": {
+                "basic": {
+                    "armor": [2, 0.161],
+                    "dodge": [5],
+                    "hit": [5],
+                    "hp": [55, 0.555],
+                    "pow": [16],
+                    "rate": [45],
+                    "speed": [10]
+                },
+                "grow": {
+                    "dodge": [0.303, 0],
+                    "hit": [0.303, 0],
+                    "pow": [0.242, 0],
+                    "rate": [0.181, 0]
+                }
+            }
+        }
+        self.doll_data = Doll.objects.filter(id__in=[item['id'] for item in self.data]).prefetch_related(
             'effect_set', 'effect_set__effectgrid_set', 'status_set'
         ).values(
             'id',
@@ -38,6 +188,7 @@ class Simulator:
             'slot_01',
             'slot_02',
             'slot_03',
+            'grow',
             'status__hp',
             'status__pow',
             'status__hit',
@@ -63,9 +214,34 @@ class Simulator:
         )
         self.result = []
 
-    @property
-    def test_return(self):
-        return self.doll_query_set
+    @cached_property
+    def default_status(self):
+        level = 100
+        for item in self.doll_data:
+            # hp & armor
+            for basic_stats in 'hp,armor'.split(','):
+
+                if level >= 100:
+                    basic_data = self.doll_grow['after100']['basic']
+                    base_data_1 = float(basic_data[basic_stats][0])
+                    level_var = float((level - 1)) * float(basic_data[basic_stats][1])
+                    attr = self.status_att[f'{item["type"].lower()}'].get(basic_stats)
+                    status = float(item[f'status__{basic_stats}'])
+                    if not attr:
+                        continue
+                    formula = math.ceil(math.ceil(base_data_1 + level_var) * math.ceil(float(attr) * status) / 100)
+
+                    if basic_stats == 'hp':
+                        item[f'status__{basic_stats}'] = formula * 5
+                    else:
+                        item[f'status__{basic_stats}'] = formula
+
+                grow_data = self.doll_grow['after100']['grow']
+                for nomal_stats in 'dodge,hit,pow,rate'.split(','):
+                    grow_data_0 = grow_data[nomal_stats][0]
+                    grow_data_1 = grow_data[nomal_stats][1]
+
+            print(item)
 
     @cached_property
     def equip_query_set(self):
@@ -77,8 +253,47 @@ class Simulator:
                 result.append(value)
         return Equip.objects.filter(id__in=result).values()
 
+    @cached_property
+    def doll_query_set(self):
+        data_set = []
+        for index, item in enumerate(self.doll_data):
+            data = {
+                'position': self.data[index]['position'],
+                'position_xy': [],
+                'id': self.data[index]['id'],
+                'doll_info': item,
+            }
+            data_set.append(data)
+        return data_set
+
+    def simulator_result(self):
+        return Status(self.doll_query_set).formula
+
 
 class Positions:
-    def __init__(self, **kwargs):
-        self.position = kwargs['position']
-        self.pow = kwargs['pow']
+    def __init__(self, query):
+
+        self.query_set = query
+        self.relative_position = {
+            1: (-1, 1),
+            2: (0, 1),
+            3: (1, 1),
+            4: (-1, 0),
+            5: (0, 0),
+            6: (1, 0),
+            7: (-1, -1),
+            8: (0, -1),
+            9: (1, -1),
+        }
+        self.result = []
+
+    def get_position(self):
+        for data in self.query_set:
+            position_list = data['doll_info']['effect__pos']
+
+            for item in position_list:
+                data['position_xy'].append((
+                    self.relative_position[data['position']][0] - self.relative_position[item][0],
+                    self.relative_position[data['position']][1] - self.relative_position[item][1]
+                ))
+        return self.query_set
